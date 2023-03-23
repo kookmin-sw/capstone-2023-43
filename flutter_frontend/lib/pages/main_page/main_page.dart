d2870742cfb0e7299d31f120bcfc38edf4bcb8f7
cocozo <kjh062627@gmail.com>
2023-03-23 19:10:01 +0900

    create flutter base component && protype main page

---
+++ flutter_frontend/lib/pages/main_page/main_page.dart
@@ -0,0 +1,117 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_frontend/widgets/base_button.dart';
+import 'package:flutter_frontend/widgets/base_item.dart';
+import 'package:flutter_frontend/widgets/base_widget.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+// 나중에 서비스가 생성되면 HookWidget 에서 HookConsumerWidget으로 변경 필요.
+class MainPage extends HookWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseWidget(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 175,
+            backgroundColor: Color.fromRGBO(11, 106, 227, 1),
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                alignment: Alignment.bottomCenter,
+                decoration: const BoxDecoration(
+                  color: Color(0x0B6AE3),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 50),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      BaseButton(
+                        icon: const Icon(Icons.calendar_month),
+                        text: '달력보기',
+                        onTap: () => print('clicked!'),
+                      ),
+                      const SizedBox(width: 20),
+                      BaseButton(
+                        icon: const Icon(Icons.add),
+                        text: '먹고있는 약 추가하기',
+                        onTap: () => print('clicked!'),
+                      ),
+                      const SizedBox(width: 20),
+                      BaseButton(
+                        icon: const Icon(Icons.search),
+                        text: '약 검색하기',
+                        onTap: () => print('clicked!'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            title: Text('PillBox'),
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(0),
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      topRight: Radius.circular(25),
+                    )),
+                width: double.maxFinite,
+                height: 30,
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                  // message 구현을 위한 모델 필요 -> isMessage?
+                  (context, index) => index == 0
+                      ? Padding(
+                          padding: const EdgeInsets.only(bottom: 20),
+                          child: BaseItem(child: Text('progress bar')),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.only(bottom: 20),
+                          child:
+                              BaseItem(child: Text('test pill before $index')),
+                        ),
+                  childCount: 4),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(50, 0, 50, 50),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                  // message 구현을 위한 모델 필요 -> isMessage?
+                  (context, index) => index == 0
+                      ? const Padding(
+                          padding: EdgeInsets.only(bottom: 20),
+                          child: Center(
+                              child: Text(
+                            '오늘도 열심히 드시고 계시네요! 💪 ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: Color.fromRGBO(165, 165, 165, 1)),
+                          )),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.only(bottom: 20),
+                          child:
+                              BaseItem(child: Text('test pill after $index')),
+                        ),
+                  childCount: 3),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
