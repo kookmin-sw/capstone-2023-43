f463b5c9624c625a300e4bd1f5bc03d6a2a35213
park jongheum <whdgmawkd@naver.com>
2023-03-23 16:28:13 +0900

    asyncio concurrent 갯수 제한 코드 추가

--- Crawler/async_task.py
+++ Crawler/async_task.py
@@ -5,6 +5,14 @@ async def request(page):
     await asyncio.sleep(random.random())
     return page
 
+async def request_sem(page, sem: asyncio.Semaphore):
+    await sem.acquire() # wait semaphore
+    await asyncio.sleep(random.random())
+    # 실제 코드 입력
+    sem.release() # release semaphore, 코루틴 아님 await 쓰는거 아님
+    return page
+
+
 async def main():
     # method 1
     ret = await asyncio.gather(*[request(page) for page in range(100)])
@@ -20,6 +28,13 @@ async def main():
     for task in done:
         result = await task
         print(result)
+
+    # method 3
+    # limit maximum concurrent requests
+    concurrent_count = 5 # maximum 5 concurrent requests
+    aio_sem = asyncio.Semaphore(concurrent_count)
+    ret = await asyncio.gather(*[request_sem(page, aio_sem) for page in range(100)])
+    print(ret)
 
 if __name__ == "__main__":
     asyncio.run(main())
