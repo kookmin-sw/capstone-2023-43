d5fcbd33102a42b954c1a5d477edc8d07afef8f8
park jongheum <whdgmawkd@naver.com>
2023-03-26 04:21:36 +0900

    복용금기 크롤러 리팩토링

--- Crawler/dur_taboo.py
+++ Crawler/dur_taboo.py
@@ -1,33 +1,39 @@
 import asyncio
-from asyncio import Semaphore
-import aiohttp
+from async_request_manager import RequestManager
 import filter_options
 import json
 
 async def main():
-    sem = Semaphore(32)
-    dur_data = list()
+
+    request_manager = RequestManager(50)
     request_url = filter_options.base_url + "/DURPrdlstInfoService02/getUsjntTabooInfoList02"
-    session = aiohttp.ClientSession()
-    result = await asyncio.gather(*[request(session, request_url, {"serviceKey":filter_options.token, "pageNo": page, "numOfRows": filter_options.NUM_OF_ROWS, "type": "json"}, sem) for page in range(1, 465871//filter_options.NUM_OF_ROWS + 1)])
-    await session.close()
-    for response in result:
-        for item in response['body']['items']:
-            dur_data.append({"item_seq": item['ITEM_SEQ'], "mixture_item_seq": item['MIXTURE_ITEM_SEQ'], "prohibited_content": item['PROHBT_CONTENT']})
-        print(f"page: {response['body']['pageNo']}. processed: {len(response['body']['items'])}. total: {len(dur_data)}")
-    # ret = await request(session, request_url, {"serviceKey":filter_options.token, "pageNo": 1, "numOfRows": filter_options.NUM_OF_ROWS, "type": "json"}, sem)
-    # for item in ret['body']['items']:
-    #     print(item['DUR_SEQ'], item['ITEM_SEQ'], item['MIXTURE_ITEM_SEQ'], item['PROHBT_CONTENT'])
+
+    await request_manager.start()
+
+    for page in range(1, 465871//filter_options.NUM_OF_ROWS + 1):
+        await request_manager.create_request(
+            request_url,
+            {"serviceKey":filter_options.token, "pageNo": page, "numOfRows": filter_options.NUM_OF_ROWS, "type": "json"}
+        )
+    
+    retry = 0
+    dur_data = list()
+    while retry < 3:
+        url, _, _, response_json = await request_manager.get_response()
+        if url is None:
+            retry += 1
+            continue
+        # filter chart
+        for item in response_json['body']['items']:
+            for chart_filter in filter_options.full_charts:
+                if chart_filter in item['CHART'] and chart_filter in item['MIXTURE_CHART']: # 둘다 필터에 맞으면
+                    dur_data.append({"item_seq": item['ITEM_SEQ'], "mixture_item_seq": item['MIXTURE_ITEM_SEQ'], "prohibited_content": item['PROHBT_CONTENT']})
+                    print(f"{item['ITEM_SEQ']} {item['ITEM_NAME']} {item['MIXTURE_ITEM_SEQ']} {item['MIXTURE_ITEM_NAME']}")
+                    break
+    await request_manager.stop()
+    
     with open("dur_output.json", "w") as file:
         json.dump({"data": dur_data}, file)
-
-async def request(session: aiohttp.ClientSession, request_url: str, options: dict, sem: Semaphore):
-    await sem.acquire()
-    response = await session.get(request_url, params=options)
-    json_response = await response.json()
-    sem.release()
-    print(f"page: {options['pageNo']}")
-    return json_response
 
 if __name__ == "__main__":
     asyncio.run(main())
