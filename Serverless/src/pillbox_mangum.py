dfa24d85b15df461d4e3df320eba2a959319c331
park jongheum <whdgmawkd@naver.com>
2023-05-01 19:30:27 +0900

    mangum 동작하지 않는 문제 해결

--- Serverless/src/pillbox_mangum.py
+++ Serverless/src/pillbox_mangum.py
@@ -1,11 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from starlette.requests import Request
+from starlette.responses import JSONResponse
 from mangum import Mangum
+from starlette.exceptions import HTTPException as StarletteHttpException
 
 app = FastAPI()
 
-@app.get("/")
+@app.exception_handler(StarletteHttpException)
+def error_handler(request: Request, exc):
+    print(request.url)
+    return JSONResponse(
+        status_code=404,
+        content={"error": "not found"}
+    )
+
+@app.get("/pillbox/test")
 def hello_world(request: Request):
-    return {"aws": request.scope["aws"]}
+    print(request.url)
+    return {"aws": "5678"}
+
+@app.post("/pillbox")
+def hello_world(request: Request):
+    print(request.url)
+    return {"aws": "1234"}
 
 handler = Mangum(app)
