df3d45ff882d59742969c54c2f16832d3399b654
fmowl10 <jinseok1001@outlook.com>
2023-05-02 23:18:59 +0900

    pill_history 관련 CRUD 부분 완료

--- Serverless/src/pillbox_mangum.py
+++ Serverless/src/pillbox_mangum.py
@@ -6,10 +6,11 @@ from mangum import Mangum
 from starlette.exceptions import HTTPException as StarletteHttpException
 from pymongo import MongoClient
 from pydantic import BaseModel
+from bson import ObjectId
 from typing import Union, List
 from bson import json_util
 import json
-import time
+from datetime import datetime
 
 
 class PillHistory(BaseModel):
@@ -35,7 +36,11 @@ pillbox_db = client['pillbox']['pillbox']
 app = FastAPI()
 
 def get_user_id(scope):
-    return scope['aws.event']['requestContext']['authorizer']['claims']['sub']
+    try:
+        user_id = scope['aws.event']['requestContext']['authorizer']['claims']['sub']
+    except KeyError:
+        user_id = None
+    return user_id
 
 def is_exist(user_id: str):
     result = pillbox_db.find_one({"_id": user_id})
@@ -52,15 +57,19 @@ def error_handler(request: Request, _):
 
 @app.get('/pillbox/users')
 def get_user(request: Request):
-    print(request.headers)
-    print(request.scope['aws.event'])
     user_id = get_user_id(request.scope)
+    if user_id is None:
+        return {"result": "Unauthoriazation"}
     user_doc = pillbox_db.find_one({"_id": user_id})
     return {"result": "ok", "datas": [user_doc]}
 
 @app.post('/pillbox/users')
 def post_user(user: User, request: Request):
     user_id = get_user_id(request.scope)
+
+    if user_id is None:
+        return {"result": "Unauthoriazation"}
+
     if is_exist(user_id):
         return {"result": "already exists"}
 
@@ -72,6 +81,8 @@ def post_user(user: User, request: Request):
 @app.put('/pillbox/users')
 def put_user(user: User, request: Request):
     user_id = get_user_id(request.scope)
+    if user_id is None:
+        return {"result": "Unauthoriazation"}
     if not is_exist(user_id):
         return {"result": "not exist User"}
 
@@ -80,26 +91,67 @@ def put_user(user: User, request: Request):
     return {"result": "ok"}
 
 @app.get('/pillbox/users/pill_histories')
-def get_pill_histories(request: Request, name: str = ''):
+def get_pill_histories(request: Request, name: str = None, all_histories: bool = False, only_ended_histories: bool = False):
     user_id = get_user_id(request.scope)
 
+    if user_id is None:
+        return {"result": "Unauthoriazation"}
+
+    if not is_exist(user_id):
+        return {"result": "No User Data"}
+
+    if all_histories and only_ended_histories:
+        return {"result": "choose one parameter from all_histories and only_ended_histories"}
+
     match = {"$match": {}}
-    if len(name) > 0:
+    if name is not None:
         match["$match"]["pill_histories.name"] = name
-    
-    group = {"$group": {"_id": "$_id", "datas":{"$addToSet": "$pill_histories"}}}
+    if not all_histories and not only_ended_histories:
+        match["$match"]["pill_histories.end_date"] = {"$gte": datetime.utcnow()}
+    elif only_ended_histories:
+        match["$match"]["pill_histories.end_date"] = {"$lt": datetime.utcnow()}
+
+    group = {"$group": {"_id": "$_id", "datas": {"$addToSet": "$pill_histories"}}}
 
     results_out = pillbox_db.aggregate([{"$match": {"_id": user_id}},
                                         {"$unwind": "$pill_histories"},
                                         match,
                                         group,
-                                        {"$project": {"_id": 0,"datas._id": 0}}
+                                        {"$project": {"_id": 0}}
                                         ])
     results = json.loads(json_util.dumps(results_out))
     results = results[0]['datas'] if len(results) > 0 else []
-    return {"msg": "ok", 'datas': results}
+    return {"result": "ok", "datas": results}
 
-# @app.get('/pillbox/users/pill_histories/:history_id')
+@app.get('/pillbox/users/pill_histories/{history_id}')
+def get_pill_histories_by_id(request: Request, history_id: str):
+    user_id = get_user_id(request.scope)
+
+    if user_id is None:
+        return {"result": "Unauthorization"}
+
+    result_out = pillbox_db.find_one({"_id": user_id, 
+                                      "pill_histories._id": ObjectId(history_id)})
+
+    results = json.loads(json_util.dumps(result_out))
+    result = [result for result in results['pill_histories'] if result['_id']['$oid'] == history_id][0]
+
+    return {"result": "ok", "datas": result}
+
+@app.post('/pillbox/users/pill_histories')
+def post_pill_histories(request: Request, pill_history: PillHistory):
+    pass
+
+@app.put('/pillbox/users/pill_histories/{history_id}')
+def update_pill_histories_by_id(request: Request, pill_history: PillHistory, history_id: str):
+    user_id = get_user_id(request.scope)
+
+    if user_id is None:
+        return {"result": "Unauthorization"}
+    
+    if not is_exist(user_id):
+        return {"result": "No User Data"}
+
 
 
 handler = Mangum(app)
