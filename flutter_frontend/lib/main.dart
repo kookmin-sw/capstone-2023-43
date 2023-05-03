2aae7a819fa451618de8e99af819dc4c0b686f54
cocozo <kjh062627@gmail.com>
2023-05-03 21:30:57 +0900

    aws amplify 적용 완료,이에 따른 종속성변경 및 세부 사항 조정

--- flutter_frontend/lib/main.dart
+++ flutter_frontend/lib/main.dart
@@ -6,16 +6,45 @@ import 'package:flutter_frontend/service/grapgql_config.dart';
 import 'package:flutter_frontend/service/pb_graph_client.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_frontend/amplifyconfiguration.dart';
 
 PbGraphQlClient gq = PbGraphQlClient();
 void main() async {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
   // This widget is the root of your application.
+
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _configure();
+  }
+
+  void _configure() async {
+    final configured;
+    final auth = AmplifyAuthCognito();
+    final analytics = AmplifyAnalyticsPinpoint();
+
+    try {
+      Amplify.addPlugins([auth, analytics]);
+      await Amplify.configure(amplifyconfig);
+      print('Successfully configured Amplify 🎉');
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +62,7 @@ class MyApp extends StatelessWidget {
             theme: ThemeData(
                 fontFamily: 'NotoSansKR',
                 scaffoldBackgroundColor: Color.fromRGBO(255, 255, 255, 1)),
-            home: const MainPage(),
+            home: ProviderScope(child: const MainPage()),
           ),
         );
       },
