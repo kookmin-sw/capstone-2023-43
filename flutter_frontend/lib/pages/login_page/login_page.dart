08123aea79e59511c169dbe7aeb93dfb620bd7de
cocozo <kjh062627@gmail.com>
2023-05-22 16:10:30 +0900

    로그인 페이지 간단구성

--- flutter_frontend/lib/pages/login_page/login_page.dart
+++ flutter_frontend/lib/pages/login_page/login_page.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_frontend/widgets/base_button.dart';
 import 'package:flutter_frontend/widgets/base_widget.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../amplifyconfiguration.dart';
 import '../../service/http_response_service.dart';
 
 class LoginPage extends HookConsumerWidget {
@@ -50,35 +50,66 @@ class LoginPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     var isLoading = useState(false);
     return BaseWidget(
-      body: Center(
-        child: isLoading.value
-            ? CircularProgressIndicator()
-            : BaseButton(
-                text: 'click me! to google login this app!',
-                onTap: () async {
-                  isLoading.value = true;
-                  var isSignedIn = await Login();
-                  if (isSignedIn) {
-                    var token = await getToken();
-                    ref.read(HttpResponseServiceProvider).setToken(token);
-                    await ref.read(HttpResponseServiceProvider).initResponse();
-                    if (ref.read(HttpResponseServiceProvider).stage ==
-                        ResposeStage.newUser) {
-                      await ref.read(HttpResponseServiceProvider).postNewUser(
-                          "김재하",
-                          "M",
-                          DateTime.utc(1999, 6, 26),
-                          0,
-                          false,
-                          false);
-                    }
-                    if (ref.read(HttpResponseServiceProvider).stage ==
-                        ResposeStage.ready) {
-                      Navigator.pushReplacementNamed(context, '/main');
-                    }
-                  }
-                },
-              ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(50.w, 20.h, 50.w, 0),
+        child: Center(
+          child: isLoading.value
+              ? CircularProgressIndicator()
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'PillBox',
+                      style: TextStyle(
+                        fontSize: 84.sp,
+                        fontWeight: FontWeight.w700,
+                        color: const Color.fromRGBO(11, 106, 227, 1),
+                      ),
+                    ),
+                    Text(
+                      '당신의 복약기록 도우미',
+                      style: TextStyle(
+                        fontSize: 30.sp,
+                        fontWeight: FontWeight.w700,
+                        color: const Color.fromRGBO(11, 106, 227, 1),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 140.h,
+                    ),
+                    BaseButton(
+                      text: '구글로 로그인하기',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.sp,
+                          fontWeight: FontWeight.w700),
+                      color: const Color.fromRGBO(11, 106, 227, 1),
+                      onTap: () async {
+                        isLoading.value = true;
+                        var isSignedIn = await Login();
+                        if (isSignedIn) {
+                          var token = await getToken();
+                          ref.read(HttpResponseServiceProvider).setToken(token);
+                          await ref
+                              .read(HttpResponseServiceProvider)
+                              .initResponse();
+                          if (ref.read(HttpResponseServiceProvider).stage ==
+                              ResposeStage.newUser) {
+                            await ref
+                                .read(HttpResponseServiceProvider)
+                                .postNewUser("김재하", "M",
+                                    DateTime.utc(1999, 6, 26), 0, false, false);
+                          }
+                          if (ref.read(HttpResponseServiceProvider).stage ==
+                              ResposeStage.ready) {
+                            Navigator.pushReplacementNamed(context, '/main');
+                          }
+                        }
+                      },
+                    ),
+                  ],
+                ),
+        ),
       ),
     );
   }
